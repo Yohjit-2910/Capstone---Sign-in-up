612a28f6fd98548c91af5eafde3c5eeb21424830
Yohjit Chopra <103960257+Yohjit-2910@users.noreply.github.com>
2023-08-02 01:22:25 +1000

    Change made for JS script to work
    Some Error, JS Script not working
    Dipansh Please check

--- web/script.js
+++ web/script.js
@@ -1,5 +1,8 @@
 const emailInput = document.getElementById('email');
 const switchInput = document.getElementById('switchInput');
+const usernameInput = document.getElementById('usernameInput');
+const passwordInput = document.getElementById('passwordInput');
+const loginBtn = document.getElementById('loginBtn');
 
 switchInput.addEventListener('change', () => {
   if (switchInput.checked) {
@@ -8,5 +11,19 @@ switchInput.addEventListener('change', () => {
   } else {
     emailInput.setAttribute('type', 'email');
     emailInput.setAttribute('placeholder', 'Email ID');
+  }
+});
+
+loginBtn.addEventListener('click', () => {
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  // Check if username and password are "admin"
+  if (username === 'admin' && password === 'admin') {
+    // Redirect to welcome.html
+    window.location.href = 'welcome.html';
+  } else {
+    // Add an error message or handle incorrect credentials as desired
+    console.log('Invalid username or password');
   }
 });
