96b91e6d96c17d7ca7cac7f7e961c6fcdfd3c13d
Yohjit Chopra <103960257+Yohjit-2910@users.noreply.github.com>
2023-08-02 01:28:12 +1000

    Error Solved - Manual Sign Up
    Well done Dipansh

--- web/script.js
+++ web/script.js
@@ -1,8 +1,7 @@
 const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
 const switchInput = document.getElementById('switchInput');
-const usernameInput = document.getElementById('usernameInput');
-const passwordInput = document.getElementById('passwordInput');
-const loginBtn = document.getElementById('loginBtn');
+const loginForm = document.getElementById('login-form');
 
 switchInput.addEventListener('change', () => {
   if (switchInput.checked) {
@@ -14,16 +13,18 @@ switchInput.addEventListener('change', () => {
   }
 });
 
-loginBtn.addEventListener('click', () => {
-  const username = usernameInput.value;
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent form submission to handle login logic manually
+
+  const emailOrUsername = emailInput.value;
   const password = passwordInput.value;
 
-  // Check if username and password are "admin"
-  if (username === 'admin' && password === 'admin') {
-    // Redirect to welcome.html
+  // Replace the following condition with your actual authentication logic
+  if (emailOrUsername === 'admin' && password === 'admin') {
+    // Redirect to welcome.html (change the URL to your welcome page)
     window.location.href = 'welcome.html';
   } else {
     // Add an error message or handle incorrect credentials as desired
-    console.log('Invalid username or password');
+    console.log('Invalid email/username or password');
   }
 });
