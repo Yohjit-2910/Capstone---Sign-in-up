6a83452017a702d27bf65bca28926d86678413c9
dipanshgarg21 <dipanshgarg21@gmail.com>
2023-08-02 02:15:54 +1000

    Added database connection

--- web/script.js
+++ web/script.js
@@ -1,3 +1,4 @@
+const { MongoClient } = require('mongodb');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const switchInput = document.getElementById('switchInput');
@@ -12,6 +13,33 @@ switchInput.addEventListener('change', () => {
     emailInput.setAttribute('placeholder', 'Email ID');
   }
 });
+
+const url = 'mongodb+srv://yohjit:2910@khfm2a1.mongodb.net/testCredentials?retryWrites=true&w=majority';
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    return;
+  }
+
+  const db = client.db('test');
+  console.log('Connected to MongoDB!');
+
+  const collection = db.collection('testCredentials');
+
+  const query = { username: emailOrUsername, password: password };
+
+  collection.find(query).toArray((err, documents) => {
+    if (err) {
+      console.error('Error fetching documents:', err);
+    } else {
+      console.log('Found documents:', documents);
+    }
+
+    client.close();
+  });
+});
+
+
 
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevent form submission to handle login logic manually
